2321e258de7b173900ed486cee32b692e2fb1ced
Manuel Gauto <mgauto@mgenterprises.org>
2014-05-15 01:55:44 -0400

    Initial Commit

---
+++ src/org/mgenterprises/mgmoney/invoice/Invoice.java
@@ -0,0 +1,68 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2014 MG Enterprises Consulting LLC.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package org.mgenterprises.mgmoney.invoice;
+
+import org.mgenterprises.mgmoney.Customer;
+
+/**
+ *
+ * @author Manuel Gauto
+ */
+public class Invoice {
+    private int invoiceNumber;
+    private Customer customer;
+    private InvoiceItem[] invoiceItem;
+
+    public Invoice(int invoiceNumber, Customer customer) {
+        this.invoiceNumber = invoiceNumber;
+        this.customer = customer;
+    }
+
+    public int getInvoiceNumber() {
+        return invoiceNumber;
+    }
+
+    public void setInvoiceNumber(int invoiceNumber) {
+        this.invoiceNumber = invoiceNumber;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public InvoiceItem[] getInvoiceItem() {
+        return invoiceItem;
+    }
+
+    public void setInvoiceItem(InvoiceItem[] invoiceItem) {
+        this.invoiceItem = invoiceItem;
+    }
+    
+    
+}
