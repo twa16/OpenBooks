13657bece96293b197aa0c08b513b9bd393cdf10
Manuel Gauto <mgauto@mgenterprises.org>
2015-06-25 18:19:08 -0400

    Cleanup old setup stuff. Start on startup splash. Start on Check Tracking.

---
+++ src/main/java/check/Check.java
@@ -0,0 +1,97 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2015 MG Enterprises Consulting LLC.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package check;
+
+import java.util.Date;
+import org.mgenterprises.openbooks.customer.Customer;
+import org.mgenterprises.openbooks.saving.Saveable;
+
+/**
+ * Represent a check received
+ * @author mgauto
+ */
+public class Check extends Saveable{
+    private long id;
+    private Customer customer;
+    private Date dateReceived;
+    private String poNumber;
+    private String checkNmber;
+    private double amount;
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Date getDateReceived() {
+        return dateReceived;
+    }
+
+    public void setDateReceived(Date dateReceived) {
+        this.dateReceived = dateReceived;
+    }
+
+    public String getPoNumber() {
+        return poNumber;
+    }
+
+    public void setPoNumber(String poNumber) {
+        this.poNumber = poNumber;
+    }
+
+    public String getCheckNmber() {
+        return checkNmber;
+    }
+
+    public void setCheckNmber(String checkNmber) {
+        this.checkNmber = checkNmber;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+    
+    @Override
+    public String getSaveableModuleName() {
+        return this.getClass().getName();
+    }
+
+    @Override
+    public String getUniqueId() {
+        return String.valueOf(id);
+    }
+
+    @Override
+    public void setUniqueId(String id) {
+        this.id = Long.parseLong(id);
+    }
+    
+}
